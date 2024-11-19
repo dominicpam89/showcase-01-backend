7c10a6f453fca584dd0b45bbe64b0cb0808eb80d
dominicpam89 <dominicpam89@gmail.com>
2024-11-19 19:02:18 +0700

    establish function get for users and products

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 const PORT = process.env.APP_PORT || 3000
+
+const jsonErrorHandler = (err, res, message="Some error occured while fetch data")=>{
+   if(err){
+       res.status(500).send({error: true, message})
+   }
+}
+
+app.get("/api/users", (_, res)=>{
+    const filePath = path.join(__dirname, "src/dummy/users.json")
+    res.sendFile(filePath, (err)=>jsonErrorHandler(err, res, "Unable to get users data"))
+})
+
+app.get("/api/products", (_, res)=>{
+    const filePath = path.join(__dirname, "src/dummy/products.json")
+    res.sendFile(filePath, (err)=>jsonErrorHandler(err, res, "Unable to get products data"))
+})
 
 app.listen(PORT, ()=>{
     console.log(`Running on PORT: ${PORT}`)
