20c4152a85170e90b6fb62b36fa71bb281689417
dominicpam89 <dominicpam89@gmail.com>
2024-11-19 19:57:28 +0700

    establish get methods for users and products

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const express = require("express")
-const path = require("path")
+const dummyUsers = require("./src/dummy/users.json")
+const dummyProducts = require("./src/dummy/products.json")
 
 const app = express()
 const PORT = process.env.APP_PORT || 3000
 
-const jsonErrorHandler = (err, res, message="Some error occured while fetch data")=>{
-   if(err){
-       res.status(500).send({error: true, message})
-   }
-}
-
 app.get("/api/users", (_, res)=>{
-    const filePath = path.join(__dirname, "src/dummy/users.json")
-    res.sendFile(filePath, (err)=>jsonErrorHandler(err, res, "Unable to get users data"))
+    res.send({error:false, users: dummyUsers})
+})
+
+app.get("/api/users/:id", (req, res)=>{
+    const {id} = req.params
+    if(isNaN(id)) res.status(400).send({error:true, message:"Id must be number"})
+    const user = dummyUsers.find(us=>us.id==id)
+    if(!user) res.status(201).send({error:true, message:"User not found"})
+    res.send({error:false, user})
 })
 
 app.get("/api/products", (_, res)=>{
-    const filePath = path.join(__dirname, "src/dummy/products.json")
-    res.sendFile(filePath, (err)=>jsonErrorHandler(err, res, "Unable to get products data"))
+    res.send({error:false, products: dummyProducts})
+})
+
+app.get("/api/products/:id", (req, res)=>{
+    const {id} = req.params
+    if(isNaN(id)) res.status(400).send({error:true, message:"Id must be number"})
+    const product = dummyProducts.find(prod=>prod.id==id)
+    if(!product) res.status(201).send({error:true, message:"Product not found"})
+    res.send({error:false, product})
 })
 
 app.listen(PORT, ()=>{
